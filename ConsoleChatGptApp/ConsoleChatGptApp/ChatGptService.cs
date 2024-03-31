2ca775ab97efa7e7f952a6ead99d5b5d31bbefac
mohamed_benayed_93 <mohamed.ben.ayed@hotmail.com>
2024-03-31 19:26:45 +0200

    initiaze chatgpt first app console

---
+++ ConsoleChatGptApp/ConsoleChatGptApp/ChatGptService.cs
@@ -0,0 +1,29 @@
+﻿namespace ConsoleChatGptApp
+{
+    public class ChatGptService
+    {
+        public string GetResultChatGpt(string question)
+        {
+            //Please use your OpenAIAPI. This one is deleted
+            string result  = string.Empty;
+
+            var openAIAPI = new OpenAI_API.OpenAIAPI("openapikey");
+
+            var completions = openAIAPI.Completions.CreateCompletionAsync(
+               prompt: question,
+               model: "gpt-3.5-turbo-instruct",
+               max_tokens: 100,
+               temperature: 0.5f
+               );
+
+            var completionsResult = completions.Result.Completions;
+
+            if (completionsResult is not null && completionsResult.Count > 0)
+            {
+                return result = completionsResult[0].Text;
+            }
+
+            return result;
+        }
+    }
+}
---
+++ ConsoleChatGptApp/ConsoleChatGptApp/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using ConsoleChatGptApp;
+
+ChatGptService chatGptService = new ChatGptService();
+
+while(true)
+{
+    Console.Write("- ");
+    var question = Console.ReadLine();
+
+    var result = chatGptService.GetResultChatGpt(question);
+
+    Console.WriteLine(result);
+    Console.WriteLine("\n");
+}
